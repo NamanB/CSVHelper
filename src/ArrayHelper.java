64baf1c4f9dd685d9557aba42a99d4ed4ec0ca53
nbhargava <namanb.edu@gmail.com>
2016-10-12 10:22:13 -0700

    Renamed the project and the class inside it

--- src/ArrayHelper.java
+++
@@ -1,8 +0,0 @@
-/***
- * This is a helper class for 2d arrays
- * @author naman
- *
- */
-public class ArrayHelper {
-	
-}
---
+++ src/CSVData.java
@@ -0,0 +1,8 @@
+/***
+ * A class to read/write numerical CSV files and allow easy access
+ * @author Naman
+ *
+ */
+public class CSVData {
+	
+}
