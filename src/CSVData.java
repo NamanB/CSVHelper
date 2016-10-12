290359c8c02f51ecb3e3308f2c17fed35e98d79f
nbhargava <namanb.edu@gmail.com>
2016-10-12 10:40:29 -0700

    Added the method stubs for the readCVSFile

--- src/CSVData.java
+++ src/CSVData.java
@@ -1,8 +1,37 @@
 /***
  * A class to read/write numerical CSV files and allow easy access
+ * 
  * @author Naman
  *
  */
 public class CSVData {
+	private double[][] data;
+	private String[] columnNames;
+	
+	/***
+	 * Returns a new CVSData object for a file ignoring lines at the top. 
+	 * All other data is stored as doubles.
+	 * 
+	 * @param filename the file to read
+	 * @param numLinesToIgnore number of lines at the top to ignore
+	 * @param colunmNames the names of the columns
+	 * @return a CVSData object for that file
+	 */
+	public CSVData readCSVFile(String filename, int numLinesToIgnore, String[] colunmNames) {
+		return null;
+	}
+	
+	/***
+	 * Returns a new CVSData object for a file ignoring lines at the top. 
+	 * It uses the first row as the column names. All other data is stored 
+	 * as doubles.
+	 * 
+	 * @param filename the file to read
+	 * @param numLinesToIgnore number of lines at the top to ignore
+	 * @return a CVSData object for that file
+	 */
+	public CSVData readCSVFile(String filename, int numLinesToIgnore) {
+		return null;
+	}
 	
 }
